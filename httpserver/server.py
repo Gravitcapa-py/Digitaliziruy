33509c9d247f6f56e801b00a771fe18f6990b1f2
Gravitcapa-py <gravitcapa.py@gmail.com>
2021-10-30 11:13:01 +0300

    fixed js error

---
+++ httpserver/server.py
@@ -0,0 +1,22 @@
+import socket
+
+server = socket.create_server(("127.0.0.2", 8000))
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.listen(10)
+try:
+    while True:
+        client_socket, address = server.accept()
+        received_data = client_socket.recv(1024).decode("utf-8")
+
+        print("Received data from socket", received_data)
+
+        path = received_data.split(" ")[1]
+
+        response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n" \
+                   f"Hello!<br />Path: {path}"
+        client_socket.send(response.encode("utf-8"))
+        client_socket.shutdown(socket.SHUT_RDWR)
+except KeyboardInterrupt:
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
